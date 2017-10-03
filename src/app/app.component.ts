4390b65c714daf9d0b67612bb36b4c80751b3c4d
Nuttertools <m.nuttasuk@gmail.com>
2017-10-03 10:56:13 +0700

    template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
 import { Component } from '@angular/core';
 
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('resize-side-menu', [
+      state('large', style({
+        width: '200px'
+      })),
+      state('small', style({
+        width: '64px'
+      })),
+      transition('small => large', animate('100ms', keyframes([
+        style({ width: '200px' })
+      ]))),
+      transition('large => small', animate('100ms', keyframes([
+        style({ width: '64px' })
+      ]))),
+    ]),
+
+
+    trigger('resize-side-content', [
+      state('large', style({
+        width: 'calc(100% - 200px)'
+      })),
+      state('small', style({
+        width: 'calc(100% - 64px)'
+      })),
+      transition('small => large', animate('100ms', keyframes([
+        style({ width: 'calc(100% - 200px)' })
+      ]))),
+      transition('large => small', animate('100ms', keyframes([
+        style({ width: 'calc(100% - 64px)' })
+      ]))),
+    ]),
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  sideMenu = [
+    { id: 0, name: 'Profile', link: '#', icon: 'account_circle' },
+    { id: 1, name: 'Education', link: '#', icon: 'school' },
+    { id: 2, name: 'Experience', link: '#', icon: 'work' },
+    { id: 3, name: 'Skill', link: '#', icon: 'code' },
+    { id: 4, name: 'Hobby', link: '#', icon: 'videogame_asset' },
+    { id: 5, name: 'Contact Me', link: '#', icon: 'question_answer' }
+  ];
+  state: String = '';
+  selectedMenu = 0;
+
+  constructor() {
+    if (window.innerWidth > 720) {
+      this.state = 'large';
+    } else {
+      this.state = 'small';
+    }
+    window.onresize = (e) => {
+      if (window.innerWidth > 720) {
+        this.state = 'large';
+      } else {
+        this.state = 'small';
+      }
+    };
+  }
+
+
+  animateMe() {
+    // this.state = (this.state === 'small' ? 'large' : 'small');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import 'hammerjs';
+import {
+  MatIconModule,
+  MatCardModule,
+  MatListModule,
+  MatButtonModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +17,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
